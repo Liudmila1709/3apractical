4bd3e6b73a8d15c61588f16cb9a1091bd7c8fb99
Liudmila1709 <144160340+Liudmila1709@users.noreply.github.com>
2023-10-14 02:12:17 +0300

    Add files via upload

---
+++ kkk.s
@@ -0,0 +1,440 @@
+.page1 {
+    background-color: rgb(79, 42, 243);
+    width: 1450px;
+    height: 950px;
+    position: relative;
+    
+}
+.for_pic_elem {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    
+    width: 800px;
+    height: 800px;
+    
+}
+.img1 {
+    position: absolute;
+    border-radius: 50%;
+    width: 550px;
+    height: 550px;
+    margin: 0;
+    left: 0;
+    top: 0;
+   
+}
+.grid {
+    position: absolute;
+    right: 0;
+    margin-bottom:100px;
+    z-index: 20px;
+    bottom: 0;
+    
+}
+.grid_body {
+    display: grid;
+    grid-template-columns: 185px 185px 185px ;
+    grid-template-rows: 120px 150px 150px;
+    grid-template-areas: 
+    "grid_item1 grid_item1 grid_item1"
+    "grid_item2 grid_item3 grid_item4"
+    "grid_item5 grid_item6 grid_item7";
+    grid-gap: 18px;
+    
+    
+
+    
+}
+.header ,
+.grid_item2 ,
+.grid_item3 ,
+.grid_item4 ,
+.grid_item5 ,
+.grid_item6 ,
+.grid_item7 
+{
+     
+    
+     border-radius: 15px;
+}
+.header {
+    grid-area: grid_item1 ;
+    background-color: rgb(79, 42, 243);
+    grid-gap: 40px;
+    text-align: center;
+    color: rgb(255, 255, 255);
+    font-size: 60px;
+    margin-top: 10px;
+   
+
+}
+
+
+
+.grid_item2 {
+    background-color: rgb(255, 255, 255);
+    color: rgb(79, 42, 243);
+    text-align: center;
+    
+
+}
+.math {
+    margin-top: 4px;
+    margin-bottom: 13px;
+}
+.img2 {
+    width: 100px;
+    height: 100px;
+    margin-left: 5px;
+    margin-top: 10px;
+}
+
+
+
+.grid_item3 {
+    background-color: rgb(255, 255, 255);
+    color: rgb(79, 42, 243);
+    text-align: center;
+
+}
+.Physics {
+    margin-top: 4px;
+    margin-bottom: 13px;
+}
+.img3 {
+    width: 100px;
+    height: 100px;
+    margin-left: 5px;
+    margin-top: 10px;
+}
+
+
+.grid_item4 {
+    background-color: rgb(255, 255, 255);
+    color: rgb(79, 42, 243);
+    text-align: center;
+
+}
+.Medicine {
+    margin-top: 4px;
+    margin-bottom: 13px;
+   
+}
+.img4 {
+    width: 100px;
+    height: 100px;
+    margin-left: 5px;
+    margin-top: 10px;
+    margin-bottom: 0px;
+}
+
+
+
+.grid_item5 {
+    background-color: rgb(255, 255, 255);
+    color: rgb(79, 42, 243);
+    text-align: center;
+
+}
+.English {
+    margin-top: 4px;
+    margin-bottom: 13px;
+   
+}
+.img5 {
+    width: 100px;
+    height: 100px;
+    margin-left: 5px;
+    margin-top: 10px;
+    margin-bottom: 0px;
+}
+
+
+
+
+.grid_item6 {
+    background-color: rgb(255, 255, 255);
+    color: rgb(79, 42, 243);
+    text-align: center;
+}
+.img6 {
+    width: 100px;
+    height: 100px;
+    margin-left: 5px;
+    margin-top: 10px;
+    margin-bottom: 0px;
+}
+
+.Technology {
+    margin-top: 4px;
+    margin-bottom: 13px;
+}
+
+
+.grid_item7 {
+    background-color: rgb(255, 255, 255);
+    color: rgb(79, 42, 243);
+    text-align: center;
+} 
+.img7 {
+    width: 100px;
+    height: 100px;
+    margin-left: 5px;
+    margin-top: 10px;
+    margin-bottom: 0px;
+}
+.Engineering {
+    margin-top: 4px;
+    margin-bottom: 13px;
+}
+.lastp {
+    color: rgb(255, 255, 255);
+    width: 130px;
+    margin-top: 750px;
+    margin-left: 670px;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+.page2 {
+    background-color: rgb(79, 42, 243);
+    width: 1450px;
+    height: 700px;
+    
+    
+
+    
+}
+.p1 { 
+    background-color: rgb(79, 42, 243);
+    text-align: center;
+    padding-top: 30px;
+    color: rgb(253, 253, 253);
+}
+
+.para {
+    font-size: 40px;
+    margin-bottom: 0;
+}
+
+.grid2 {
+    
+}
+.grid_body2 {
+    display: grid;
+    grid-template-columns: 300px 300px 300px ;
+    grid-template-rows: 400px 400px 400px;
+    grid-gap: 18px;
+    margin-left: 250px;
+    
+    
+    
+    
+
+}
+.grid_item8 {
+    background-color: azure;
+    border-radius: 10px;
+    text-align: center;
+    
+}
+.grid_item9 {
+    background-color: azure;
+    border-radius: 10px;
+    text-align: center;
+   
+}
+.grid_item10 {
+    background-color: azure;
+    border-radius: 10px;
+    text-align: center;
+    
+}
+.img8 {
+    margin-top:20px ;
+    width: 250px;
+    height: 220px;
+   
+    border-radius: 15px;
+}
+.formore {
+    text-decoration: underline;
+    text-decoration-color: rgb(79, 42, 243);
+    font-size: 14px;
+}
+.text1page2 {
+    font-size: 20px;
+}
+
+.paragh4 {
+    color: rgb(255, 255, 255);
+    
+    width: 160px;
+    height: 50px;
+    margin-left: 380px;
+}
+
+
+
+
+
+
+
+
+
+
+
+.page3 {
+    background-color: rgb(79, 42, 243);
+    width: 1450px;
+    height: 700px;
+}
+.p3 {
+    
+    text-align: center;
+    padding-top: 30px;
+    color: rgb(253, 253, 253);
+    background-color: rgb(79, 42, 243);
+}
+.para3 {
+    font-size: 40px;
+    margin-bottom: 0;
+}
+.yellowblock {
+    position: absolute;
+    width: 1450px;
+    height: 200px;
+    background-color: gold;
+    margin-top: 100px;
+    
+}
+.for_pic_elem3 {
+    position: relative;
+}
+
+
+
+
+.grid_body3 {
+    position: absolute;
+    display: grid;
+    grid-template-columns: 280px 280px 280px ;
+    grid-template-rows: 180px 180px 180px;
+    grid-gap: 18px;
+    
+    
+    margin-left: 275px;
+    margin-bottom: 30px;
+    
+} 
+
+.ppng1 {
+    width: 30px;
+    height: 30px;
+    margin-left: 230px;
+}
+.text3pB {
+color: rgb(79, 42, 243) ;
+font-size: 18px;
+margin-left: 10px;
+}
+.text3ps {
+color: rgba(0, 0, 0, 0.603);
+font-size: 15px;
+margin-left: 10px;
+}
+.grid_item11 {
+    background-color: rgb(255, 255, 255);
+    
+    border-radius: 15px;
+}
+.grid_item12 {
+    background-color: rgb(255, 255, 255);
+    border-radius: 15px;
+}
+.grid_item13 {
+    background-color: rgb(255, 255, 255);
+    border-radius: 15px;
+}
+.grid_item14 {
+    background-color: rgb(255, 255, 255);
+    border-radius: 15px;
+}
+.grid_item15 {
+    background-color: rgb(255, 255, 255);
+    border-radius: 15px;
+}
+.grid_item16 {
+    background-color: rgb(255, 255, 255);
+    border-radius: 15px;
+}
+
+
+
+
+
+
+
+
+.page4 {
+    background-color: rgb(255, 0, 51);
+    width: 1450px;
+    height: 300px;
+
+}
+.container {
+    width: 800px;
+    height: 280px;
+    background-color: beige;
+    margin-left: 350px;
+}
+.grid4 {
+    display: grid;
+    grid-template-columns: 150px 150px ;
+    grid-template-rows: 100px 100px ;
+    
+    grid-gap: 20px;
+
+}
+.divin4-1 {
+    background-color: rgb(53, 157, 121);
+    text-align: center;
+    color: rgb(249, 249, 249);
+}
+.divin4-2 {
+    background-color: rgb(53, 157, 121);
+    text-align: center;
+    color: rgb(249, 249, 249);
+    
+}
+.divin4-3 {
+    background-color: rgb(53, 157, 121);
+    text-align: center;
+    color: rgb(249, 249, 249);
+}
+.divin4-4 {
+    background-color: rgb(53, 157, 121);
+    text-align: center;
+    color: rgb(249, 249, 249);
+}
+.big78 {
+   font-size: 45px;
+   margin: 0;
+}
+.fortext88 {
+    width: 350px;
+    height: 150px;
+    background-color: aquamarine;
+    margin-left: 40px;
+}
